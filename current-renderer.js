3df7108b6af2222f047d8e0bc4b435b617c55019
rahiggins <48138000+rahiggins@users.noreply.github.com>
2021-05-24 22:45:30 -0500

    Second attempt at update

--- current-renderer.js
+++ current-renderer.js
@@ -2,7 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-// Epochs version 0.1
+// Epochs version 0.1 
 
 // Code structure:
 //
